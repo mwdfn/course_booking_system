eab36caaa0880c364ef4ebf4267e941e9e6d7f89
Mark Woodfine-Jones <mwdfnj@gmail.com>
2022-06-30 11:50:36 +0100

    add component, controller, model, and repository classes

---
+++ src/main/java/com/build_a_course_booking_system/build_a_course_booking_system/models/Course.java
@@ -0,0 +1,4 @@
+package com.build_a_course_booking_system.build_a_course_booking_system.models;
+
+public class Course {
+}
---
+++ src/main/java/com/build_a_course_booking_system/build_a_course_booking_system/repositories/CustomerRepository.java
@@ -0,0 +1,4 @@
+package com.build_a_course_booking_system.build_a_course_booking_system.repositories;
+
+public interface CustomerRepository {
+}
