2119bcb0e4a1206b5150d7511778f87876319837
Mark Woodfine-Jones <mwdfnj@gmail.com>
2022-06-30 11:47:45 +0100

    first commit

---
+++ src/main/java/com/build_a_course_booking_system/build_a_course_booking_system/BuildACourseBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.build_a_course_booking_system.build_a_course_booking_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildACourseBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildACourseBookingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/build_a_course_booking_system/build_a_course_booking_system/BuildACourseBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.build_a_course_booking_system.build_a_course_booking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildACourseBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
