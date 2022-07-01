cbc04486c56e40f3b8edf806eb1bb95da20da795
Mark Woodfine-Jones <mwdfnj@gmail.com>
2022-07-01 10:04:59 +0100

    set up customer controller

--- src/main/java/com/build_a_course_booking_system/build_a_course_booking_system/controllers/CustomerController.java
+++ src/main/java/com/build_a_course_booking_system/build_a_course_booking_system/controllers/CustomerController.java
@@ -1,4 +1,22 @@
 package com.build_a_course_booking_system.build_a_course_booking_system.controllers;
 
+import com.build_a_course_booking_system.build_a_course_booking_system.models.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class CustomerController {
+
+    @Autowired
+    CustomerController customerController;
+
+    @GetMapping(value = "/customers")
+    public ResponseEntity<List<Customer>> getAllCustomers(){
+        return new ResponseEntity<>(customerController.findAll(), HttpStatus.OK);
+    }
 }
